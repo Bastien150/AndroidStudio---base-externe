72ac21fe163d94fcd42d967b5933db2de37934c6
Forest <bastienforest01@gmail.com>
2024-10-02 14:12:02 +0200

    iniatialized

--- app/src/main/java/fr/forestba/baseexterne/MainActivity.java
+++ app/src/main/java/fr/forestba/baseexterne/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -32,13 +35,11 @@ public class MainActivity extends AppCompatActivity {
         String ville = etDebut.getText().toString();
 
         //appeler le service web pour rechercher dans la base
-        String urlServiceWeb= "http://172.16.47.13/serveur-web/index.php?nom="+ville;
+        String urlServiceWeb= "http://172.16.47.13//serveur-web/index.php?debut="+ville;
 
         //afficher
         tvResult.setText("Communes débute par \"" + ville +  "\" : \n");
-        tvResult.append(getServerDataTexteBrut(urlServiceWeb));
-
-
+        tvResult.append(getServerDataJSON(urlServiceWeb));
     }
 
     public String getServerDataTexteBrut(String urlAJoindre){
@@ -59,11 +60,48 @@ public class MainActivity extends AppCompatActivity {
 
             while ((ligne = bufferedReader.readLine()) != null) {
                 res += " - " + ligne + "\n";
+
             }
         } catch (Exception e) {
             Log.d("Myapp", "Erreur échange avec serveur : "+ e.toString());
             return "";
         }
         return res;
+    }
+
+        public String getServerDataJSON(String urlAJoindre) {
+        //autoriser les opérations réseau sur le thread principal
+        StringBuilder res = new StringBuilder();
+        String ligne;
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        URL url;
+        try {
+            url = new URL(urlAJoindre);
+            HttpURLConnection connexion = (HttpURLConnection) url.openConnection();
+            connexion.connect();
+            InputStream inputStream = connexion.getInputStream();
+
+            //Récuperer le flux JSON
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            StringBuilder ch = new StringBuilder();
+            while ((ligne = bufferedReader.readLine()) != null){
+                ch.append(ligne);
+            }
+
+            JSONArray jsonArray = new JSONArray(ch.toString());
+
+            for (int i = 0; i < jsonArray.length(); i++){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                res.append(" - ").append(jsonObject.getString("nom")).append(" - ").append(jsonObject.getString("cp")).append(" - lat : ").append(String.format("%.3f",jsonObject.getDouble("lat"))).append(" - long : ").append(String.format("%.3f",jsonObject.getDouble("lon"))).append("\n");
+            }
+        } catch (Exception e) {
+            Log.d ("MyApp", "Erreur échange avec serveur : "+ e);
+        }
+
+        return res.toString();
     }
 }
